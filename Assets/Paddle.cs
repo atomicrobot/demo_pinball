e19d61054a0075036a6807ec7d10c9a476816a8d
Stuart Bowman <stuartsoft2012@gmail.com>
2017-04-05 02:18:30 -0400

    Whatever

--- Assets/Paddle.cs
+++ Assets/Paddle.cs
@@ -11,32 +11,58 @@ public class Paddle : MonoBehaviour {
 
     public Side paddleSide = Side.left;
     public int RotationSpeed = 100;
-    public float highEndStop = 0.0f;
-    public float lowEndStop = 40.0f;
+    public float range = 50.0f;
+    public float defaultPos = 40.0f;
+
+    int ccw;
 
     // Use this for initialization
     void Start () {
-		
-	}
+        ccw = (paddleSide == Side.left ? 1 : -1);//does paddle move counter clockwise?
+    }
 
     // Update is called once per frame
     void Update() {
-        int rotDir = 1;
         bool keyIsDown = Input.GetKey(paddleSide == Side.left ? KeyCode.LeftShift : KeyCode.RightShift);
-        rotDir = keyIsDown ? -1 : 1;
+        int rotDir = keyIsDown ? -1 : 1;
+        rotDir = rotDir * ccw;
+        float highPos = (defaultPos - (ccw * range));
+        if (highPos < 0.0f)
+        {
+            highPos = 360.0f + highPos;
+        }
 
-        if ((keyIsDown && transform.localRotation.eulerAngles.y != highEndStop) || (!keyIsDown && transform.localRotation.eulerAngles.y != lowEndStop))
+        if ((keyIsDown && transform.localRotation.eulerAngles.y != highPos) || (!keyIsDown && transform.localRotation.eulerAngles.y != defaultPos))
         {
             transform.Rotate(Vector3.up, Time.deltaTime * RotationSpeed * rotDir);
         }
 
-        if (keyIsDown && transform.localRotation.eulerAngles.y > lowEndStop)
+        if (ccw == 1)
         {
-            transform.localRotation = Quaternion.Euler(new Vector3(0,highEndStop,0));
+            if (keyIsDown && transform.localRotation.eulerAngles.y > defaultPos && transform.localRotation.eulerAngles.y < highPos)
+            {
+                transform.localRotation = Quaternion.Euler(new Vector3(transform.localRotation.x, highPos, transform.localRotation.z));
+            }
+            else if (!keyIsDown && transform.localRotation.eulerAngles.y > defaultPos && transform.localRotation.eulerAngles.y < highPos)
+            {
+                transform.localRotation = Quaternion.Euler(new Vector3(transform.localRotation.x, defaultPos, transform.localRotation.z));
+            }
         }
-        else if (!keyIsDown && transform.localRotation.eulerAngles.y > lowEndStop)
+        else if (ccw == -1)
         {
-            transform.localRotation = Quaternion.Euler(new Vector3(0, lowEndStop, 0));
+            if (keyIsDown && transform.localRotation.eulerAngles.y > highPos)
+            {
+                transform.localRotation = Quaternion.Euler(new Vector3(transform.localRotation.x, highPos, transform.localRotation.z));
+            }
+            else if (!keyIsDown && transform.localRotation.eulerAngles.y < defaultPos)
+            {
+                transform.localRotation = Quaternion.Euler(new Vector3(transform.localRotation.x, defaultPos, transform.localRotation.z));
+            }
+        }
+
+        if (ccw == -1)
+        {
+            Debug.Log(transform.localRotation.eulerAngles.y);
         }
     }
 }
