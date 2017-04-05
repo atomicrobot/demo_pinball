3a8e947d46e86bb9b45cb2a9e914f5cafac5a216
Stuart Bowman <stuartsoft2012@gmail.com>
2017-04-05 01:10:00 -0400

    One freaking paddle is done

--- Assets/Paddle.cs
+++ Assets/Paddle.cs
@@ -4,13 +4,39 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public enum Side
+    {
+        left, right
+    }
+
+    public Side paddleSide = Side.left;
+    public int RotationSpeed = 100;
+    public float highEndStop = 0.0f;
+    public float lowEndStop = 40.0f;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update() {
+        int rotDir = 1;
+        bool keyIsDown = Input.GetKey(paddleSide == Side.left ? KeyCode.LeftShift : KeyCode.RightShift);
+        rotDir = keyIsDown ? -1 : 1;
+
+        if ((keyIsDown && transform.localRotation.eulerAngles.y != highEndStop) || (!keyIsDown && transform.localRotation.eulerAngles.y != lowEndStop))
+        {
+            transform.Rotate(Vector3.up, Time.deltaTime * RotationSpeed * rotDir);
+        }
+
+        if (keyIsDown && transform.localRotation.eulerAngles.y > lowEndStop)
+        {
+            transform.localRotation = Quaternion.Euler(new Vector3(0,highEndStop,0));
+        }
+        else if (!keyIsDown && transform.localRotation.eulerAngles.y > lowEndStop)
+        {
+            transform.localRotation = Quaternion.Euler(new Vector3(0, lowEndStop, 0));
+        }
+    }
 }
