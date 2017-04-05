9ed2c8806c4b0ebdfd38514d6a783c88d6ba3705
Stuart Bowman <stuartsoft2012@gmail.com>
2017-04-05 03:41:45 -0400

    Added physics material for the ball. Also remodeld the table a little

---
+++ Assets/Ball.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        transform.GetComponent<Rigidbody>().maxAngularVelocity = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
